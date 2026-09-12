9bb4e829e98276062c5ec90652d125175f28c965
onononoo <kai.iso.torrent@gmail.com>
2026-09-12 11:47:26 -0500

    Update comment for clarity in start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @echo off
-REM Serve "dotimg" locally. ES modules need http://, not file://.
+REM Serve dotimg locally. ES modules need http://, not file://.
 cd /d "%~dp0"
 where py >nul 2>nul && (start "" http://localhost:8137 & py -m http.server 8137 & goto :eof)
 where python >nul 2>nul && (start "" http://localhost:8137 & python -m http.server 8137 & goto :eof)
